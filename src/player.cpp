16d6ef1d7f5ec6adfd3396b6c06c6207b0ff1e96
jo-qzy <2651933495@qq.com>
2021-01-05 17:32:44 +0800

    Add cmakelist.

---
+++ src/player.cpp
@@ -0,0 +1,22 @@
+//
+// Created by BoringWednesday on 2021/1/5.
+//
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <SDL.h>
+#include <SDL_video.h>
+#include <SDL_render.h>
+#include <SDL_rect.h>
+#ifdef __cplusplus
+}
+#endif
+
+
+int main()
+{
+    return 0;
+}
